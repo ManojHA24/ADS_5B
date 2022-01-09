776975c994933195904072e3dacfe52c0a790119
ManojHA24 <71307033+ManojHA24@users.noreply.github.com>
2022-01-09 08:41:29 +0530

    Update Dictionary_hashing.c

--- Dictionary/Dictionary_hashing.c
+++ Dictionary/Dictionary_hashing.c
@@ -1,5 +1,3 @@
-#define _XOPEN_SOURCE 500 /* Enable certain library functions (strdup) on linux.  See feature_test_macros(7) */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
@@ -21,7 +19,6 @@ struct hashtable_s {
 typedef struct hashtable_s hashtable_t;
 
 
-/* Create a new hashtable. */
 hashtable_t *ht_create( int size ) {
 
 	hashtable_t *hashtable = NULL;
@@ -29,12 +26,10 @@ hashtable_t *ht_create( int size ) {
 
 	if( size < 1 ) return NULL;
 
-	/* Allocate the table itself. */
 	if( ( hashtable = malloc( sizeof( hashtable_t ) ) ) == NULL ) {
 		return NULL;
 	}
 
-	/* Allocate pointers to the head nodes. */
 	if( ( hashtable->table = malloc( sizeof( entry_t * ) * size ) ) == NULL ) {
 		return NULL;
 	}
@@ -47,13 +42,11 @@ hashtable_t *ht_create( int size ) {
 	return hashtable;	
 }
 
-/* Hash a string for a particular hash table. */
 int ht_hash( hashtable_t *hashtable, char *key ) {
 
 	unsigned long int hashval;
 	int i = 0;
 
-	/* Convert our string to an integer */
 	while( hashval < ULONG_MAX && i < strlen( key ) ) {
 		hashval = hashval << 8;
 		hashval += key[ i ];
@@ -63,7 +56,6 @@ int ht_hash( hashtable_t *hashtable, char *key ) {
 	return hashval % hashtable->size;
 }
 
-/* Create a key-value pair. */
 entry_t *ht_newpair( char *key, char *value ) {
 	entry_t *newpair;
 
@@ -84,7 +76,7 @@ entry_t *ht_newpair( char *key, char *value ) {
 	return newpair;
 }
 
-/* Insert a key-value pair into a hash table. */
+
 void ht_set( hashtable_t *hashtable, char *key, char *value ) {
 	int bin = 0;
 	entry_t *newpair = NULL;
@@ -100,26 +92,21 @@ void ht_set( hashtable_t *hashtable, char *key, char *value ) {
 		next = next->next;
 	}
 
-	/* There's already a pair.  Let's replace that string. */
 	if( next != NULL && next->key != NULL && strcmp( key, next->key ) == 0 ) {
 
 		free( next->value );
 		next->value = strdup( value );
 
-	/* Nope, could't find it.  Time to grow a pair. */
 	} else {
 		newpair = ht_newpair( key, value );
 
-		/* We're at the start of the linked list in this bin. */
 		if( next == hashtable->table[ bin ] ) {
 			newpair->next = next;
 			hashtable->table[ bin ] = newpair;
 	
-		/* We're at the end of the linked list in this bin. */
 		} else if ( next == NULL ) {
 			last->next = newpair;
 	
-		/* We're in the middle of the list. */
 		} else  {
 			newpair->next = next;
 			last->next = newpair;
@@ -127,20 +114,17 @@ void ht_set( hashtable_t *hashtable, char *key, char *value ) {
 	}
 }
 
-/* Retrieve a key-value pair from a hash table. */
 char *ht_get( hashtable_t *hashtable, char *key ) {
 	int bin = 0;
 	entry_t *pair;
 
 	bin = ht_hash( hashtable, key );
 
-	/* Step through the bin, looking for our value. */
 	pair = hashtable->table[ bin ];
 	while( pair != NULL && pair->key != NULL && strcmp( key, pair->key ) > 0 ) {
 		pair = pair->next;
 	}
 
-	/* Did we actually find anything? */
 	if( pair == NULL || pair->key == NULL || strcmp( key, pair->key ) != 0 ) {
 		return NULL;
 
@@ -151,7 +135,7 @@ char *ht_get( hashtable_t *hashtable, char *key ) {
 }
 
 
-int main( int argc, char **argv ) {
+int main() {
 
 	hashtable_t *hashtable = ht_create( 65536 );
 	char *keyword[100],word[100],meaning[100];
